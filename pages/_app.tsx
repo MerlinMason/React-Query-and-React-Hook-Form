b9e40b99cd25ee13390f0575f51b9983e9d91744
Merlin Mason <info@merlinmason.co.uk>
2021-11-03 19:29:44 +0000

    Add Axios and React Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from "next/app";
+import axios from "axios";
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "tailwindcss/tailwind.css";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import Layout from "../components/Layout";
 
+const queryClient = new QueryClient();
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-    <>
+    <QueryClientProvider client={queryClient}>
         <Head>
             <title>NextProject</title>
             <link rel="icon" href="/favicon.ico" />
@@ -14,7 +19,8 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
         <Layout>
             <Component {...pageProps} />
         </Layout>
-    </>
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
 );
 
 export default App;
