57ada99e44b7aafb4a52217b5e588a922e5b3f21
Merlin Mason <info@merlinmason.co.uk>
2021-11-03 19:33:26 +0000

    Improve layout design

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,7 @@
-const Layout: React.FC = ({ children }) => <div className="bg-purple-800">{children}</div>;
+const Layout: React.FC = ({ children }) => (
+    <div className="bg-purple-800 min-h-screen">
+        <div className="max-w-[600px] mx-auto py-12">{children}</div>
+    </div>
+);
 
 export default Layout;
