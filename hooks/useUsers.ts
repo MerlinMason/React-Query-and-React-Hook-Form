e02b13bac6867a9712baadb32dcb9da9470307fe
Merlin Mason <info@merlinmason.co.uk>
2021-11-03 19:31:57 +0000

    Create useUsers hook

---
+++ hooks/useUsers.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosError } from "axios";
+import { useQuery, UseQueryResult } from "react-query";
+
+import type { Users } from "../types";
+
+const useUsers = (page: number, limit: number): UseQueryResult<Users, AxiosError> =>
+    useQuery(["users", { page, limit }], async () => {
+        const { data } = await axios.get("/api/user", { params: { page, limit } });
+        return data;
+    });
+
+export default useUsers;
