d2178e11ffa1aa12315409ba37e873372713bd09
Merlin Mason <info@merlinmason.co.uk>
2021-11-04 20:21:16 +0000

    Adds TextInputSkeleton component

---
+++ components/skeletons/forms/TextInputSkeleton.tsx
@@ -0,0 +1,10 @@
+const TextInputSkeleton: React.FC = () => {
+    return (
+        <div>
+            <div className="h-4 w-24 bg-gray-300 rounded mb-2"></div>
+            <div className="h-14 border border-gray-300 rounded"></div>
+        </div>
+    );
+};
+
+export default TextInputSkeleton;
