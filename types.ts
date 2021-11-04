f0166b883d1c0504a3204c523d77bb12cf406db9
Merlin Mason <info@merlinmason.co.uk>
2021-11-04 20:19:08 +0000

    Tidy types to reflect simpler MockAPI format

--- types.ts
+++ types.ts
@@ -1,29 +1,14 @@
 export type BackendError = { message: string };
 
-export type UserSummary = {
+export type User = {
     id: string;
-    title: "mr" | "ms" | "mrs" | "miss" | "dr" | "";
     firstName: string;
     lastName: string;
-    picture: string;
-};
-export type UserFull = UserSummary & {
-    gender: "male" | "female" | "other" | "";
+    avatar: string;
     email: string;
-    dateOfBirth: string;
-    registerDate: string;
-    phone: string;
-    location: {
-        street: string;
-        city: string;
-        state: string;
-        country: string;
-        timezone: string;
-    };
 };
+
 export type Users = {
-    data: UserSummary[];
-    limit: number;
-    page: number;
+    data: User[];
     total: number;
 };
