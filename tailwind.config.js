3add49fe4643d3a98ed706e0877526a5ff5c1484
Merlin Mason <info@merlinmason.co.uk>
2021-11-04 17:40:32 +0000

    Extend Tailwind to support radial gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
     mode: "jit",
     purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
-    theme: {},
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            },
+        },
+    },
     plugins: [],
 };
