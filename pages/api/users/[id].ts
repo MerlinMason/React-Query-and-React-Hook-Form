2962ec47119910dde625d650ca560b719c3d10b0
Merlin Mason <info@merlinmason.co.uk>
2021-11-05 12:39:07 +0000

    tidy

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse, AxiosError } from "axios";
+import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import type { User, BackendError } from "../../../types";
