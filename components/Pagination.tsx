1882e1c53fdc245fc882b6b5b201420fdd02e19a
Merlin Mason <info@merlinmason.co.uk>
2021-11-05 12:24:41 +0000

    Format

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -37,7 +37,7 @@ const Pagination: React.FC<Props> = ({
                 <span aria-label={`Current page ${currentPage}`} aria-current="true">
                     {currentPage}
                 </span>
-                of
+                {" of "}
                 <span aria-label={`Total pages ${totalPages}`}>{totalPages}</span>
             </div>
 
